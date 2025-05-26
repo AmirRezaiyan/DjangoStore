bd169ee3b1f6a7cad8b5fcb7dbed2e90f17d3d9a
unknown <ezio.rzn80@gmail.comgit config --global user.name>
2025-05-26 12:08:16 +0330

    add procfile

--- djangostore/settings.py
+++ djangostore/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -11,6 +12,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+
+DATABASES = {
+    "default": dj_database_url.config(conn_max_age=600)
+}
 
 
 INSTALLED_APPS = [
